611a505334df5fa557e6e07e27febceb79ae7aef
Lisa Leu <lis.leu@gmail.com>
2019-10-02 16:38:56 +1000

    modified schema

--- db/schema.sql
+++ db/schema.sql
@@ -9,22 +9,19 @@ CREATE TABLE eventDetails
     venue_name varchar(50) NOT NULL,
     venue_address varchar(80) NOT NULL,
     event_date varchar(50) NOT NULL,
-	PRIMARY KEY (guest_id)
+	PRIMARY KEY (event_id)
 );
 
 CREATE TABLE guestList
 (	guest_id int NOT NULL AUTO_INCREMENT,
-	event_name varchar(50) NOT NULL,
     guest_name varchar(50) NOT NULL,
     guest_email varchar(50) NOT NULL,
     rsvp_status varchar(50) DEFAULT "Awaiting response" NOT NULL,
     plus_one BOOLEAN DEFAULT false NOT NULL,
     date_sent TIMESTAMP(0) NOT NULL,
+    event_id int NOT NULL,
 	PRIMARY KEY (guest_id),
-     CONSTRAINT fk_  FOREIGN KEY (guest_id)
-    REFERENCES guestlist(guest_id)
-    ON DELETE SET NULL
-    ON UPDATE CASCADE
-    
+    FOREIGN KEY (event_id)
+    REFERENCES eventDetails(event_id)
+    ON DELETE CASCADE
 );
-
